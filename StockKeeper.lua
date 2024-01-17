26e5f4b6d2d7cfc97994cf879df8f9dcae03cd07
Erik Buter <e.t.buter@gmail.com>
2024-01-17 19:41:26 +0100

    Add stockkeeper

---
+++ StockKeeper.lua
@@ -0,0 +1,69 @@
+local bridge = peripheral.find("rsBridge")
+if not bridge then error("RS Bridge not found.") end
+print("RS Bridge initialized.")
+
+local stockFile = "stock.csv"
+local stock_list = {}
+local current_stock = {}
+
+function getStockFromFile()
+    if !fs.exists(stockFile) then
+        fs.create(stockFile)
+    end
+    local file = fs.open(stockFile, "r")
+    stock_list = textutils.unserialise(file.readAll())
+    file.close()
+end
+
+function writeStockToFile()
+    local file = fs.open(stockFile, "w")
+    file.write(textutils.serialise(stock_list))
+    file.close()
+end
+
+function getStockFromRS()
+    local items = rs.listItems()
+    for index, item in ipairs(items) do
+        if item.isCraftable then
+            current_stock[item.name] = item.amount
+        end
+    end
+end
+
+function checkStock()
+    for s in pairs(stock_list) do
+        local name = stock_list[s].name
+        local wantedAmount = stock_list[s].amount
+        local currentAmount = current_stock[name].amount
+        if currentAmount < wantedAmount then
+            rs.craftItem({name=name, count=wantedAmount-currentAmount})
+        end
+    end
+end
+
+function addBricks()
+    if stock_list.length == 0 then
+        brick = {name="minecraft:stone_bricks", amount=64}
+        table.insert(stock_list, brick)
+    end
+end
+
+function work()
+    getStockFromFile()
+    getStockFromRS()
+    addBricks()
+    checkStock()
+    writeStockToFile()
+end
+
+
+local delay = 10
+local TIMER = os.startTimer(delay)
+while true do
+    local e = {os.pullEvent()}
+    if e[1] == "timer" and e[2] == TIMER then
+        now = os.time()
+        work()
+        TIMER = os.startTimer(delay)
+    end
+end
